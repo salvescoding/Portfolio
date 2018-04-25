05724b3417f2e2f8527239db0d9ac4ff78c4678b
Sergio Alves <salves2383@gmail.com>
2018-04-25 19:10:40 +0200

    Integrated the views for new and show on the works, also integrated actions works#controller for new edit create show update

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,7 @@
 class WorksController < ApplicationController
 
+  before_action :set_work, only: [:show, :edit, :update, :destroy]
+
   def index
     @work_items = Work.all
   end
@@ -7,7 +9,41 @@ class WorksController < ApplicationController
   def show
   end
 
-  def method_name
+  def new
+    @work = Work.new
+  end
 
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path, notice: "Portfolio successfully created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @work.update(work_params)
+      redirect_to works_path, notice: "Portfolio successfully created"
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+
+
+  def work_params
+    params.require(:work).permit(:title, :subtitle, :body, :main_image, :thumb_imageg)
+  end
+
+  def set_work
+    @work = Work.find(params[:id])
   end
 end
+

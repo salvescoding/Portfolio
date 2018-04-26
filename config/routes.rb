10b8279bbd7175446dfe92314bd1ca4c56307dae
Sergio Alves <salves2383@gmail.com>
2018-04-26 16:49:33 +0200

    Customized work routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :works
+  resources :works, except: [:show]
 
+  get 'work/:id', to: 'works#show', as: 'work_show'
 
+  get 'contact', to: 'pages#contact'
+
+  get 'about-me', to: 'pages#about'
 
   resources :blogs
 
-  get 'contact', to: 'pages#contact'
-  get 'about-me', to: 'pages#about'
   root to: 'pages#home'
 end

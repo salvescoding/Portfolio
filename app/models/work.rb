df9b0235dcc15b43635eb322f6087a04c73849ce
Sergio Alves <salves2383@gmail.com>
2018-04-27 17:59:41 +0200

    Implemented image defaults for work items

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,11 @@
 class Work < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x4g00"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 
   # Custom scopes
   def self.ruby_on_rails_items

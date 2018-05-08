93d56e94b2e9977512cb9b9266c6b5474cc1f09a
Sergio Alves <salves2383@gmail.com>
2018-05-08 17:34:24 +0200

    Implemented initial design for portfolio index page

--- app/models/work.rb
+++ app/models/work.rb
@@ -9,7 +9,7 @@ class Work < ApplicationRecord
 
   def set_defaults
     self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
-    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
+    self.thumb_image ||= Placeholder.image_generator(height: "356", width: "280")
   end
 
   # Custom scopes

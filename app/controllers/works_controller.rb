f84794becfae5615187dd9e03d66d67af0c01979
Sergio Alves <salves2383@gmail.com>
2018-05-11 13:32:36 +0200

    Implemented authorization rules for portfolio controller and view for drag and drop feature

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,7 +1,7 @@
 class WorksController < ApplicationController
   before_action :set_work, only: [:show, :edit, :update, :destroy]
   layout "work"
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :edit, :update]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :edit, :update, :sort]}, site_admin: :all
 
   def index
     @works = Work.by_position

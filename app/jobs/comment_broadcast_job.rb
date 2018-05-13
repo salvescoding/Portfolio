2e8cf120127255fe721fdd8d9490436bf31db2dd
Sergio Alves <salves2383@gmail.com>
2018-05-13 19:33:47 +0200

    Implemented action cable for comments

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -5,6 +5,8 @@ class CommentBroadcastJob < ApplicationJob
     ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
   end
 
+  private
+
   def render_comment(comment)
     CommentsController.render partial: 'comments/comment', locals: { comment: comment }
   end

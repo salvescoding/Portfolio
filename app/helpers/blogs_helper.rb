298302e9246cee27b851b73b0fb8835ea568970c
Sergio Alves <salves2383@gmail.com>
2018-05-14 14:32:00 +0200

    Implemented topics links for side bar in blogs layout

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,5 +2,13 @@ module BlogsHelper
   def gravatar_helper(user)
     image_tag "https://api.adorable.io/avatars/35/abott@.io.png", style:'border-radius:50%';
   end
+
+  def topic_generator
+    topics = ''
+    Topic.all.limit(4).each do |topic|
+      topics << "<p><a href='#{topic_path(topic)}'>#{topic.title}</a></p>"
+    end
+    topics.html_safe
+  end
 end
 

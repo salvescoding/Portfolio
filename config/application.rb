df8ca7b1992f40e827a4a8eb35ee4d435381fd30
Sergio Alves <salves2383@gmail.com>
2018-05-16 21:12:09 +0200

    Update secret key base call

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Portfolio
     config.load_defaults 5.2
 
     config.eager_load_paths << "#{Rails.root}/lib"
-
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

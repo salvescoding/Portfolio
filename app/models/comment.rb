b77a2c0b5f7e4d8032cd4bd032b45dee2963ec6c
Sergio Alves <salves2383@gmail.com>
2018-05-20 16:25:55 +0200

    Upgraded styles in blogs and works layouts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-  validates :content, presence: true, length: { minimum: 15, maximum: 500 }
+  validates :content, presence: true, length: { minimum: 10, maximum: 500 }
 
   after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
